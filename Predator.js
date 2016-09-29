2cba77ec5fb09fc93f9a695f2637bbc5451d489c
Taylor Shuff <tkylesh@gmail.com>
2016-09-29 13:07:26 -0500

    completed the assignment

--- Predator.js
+++ Predator.js
@@ -6,6 +6,8 @@ var Predator = (function () {
     loadCarnivores: function (callbackToInvoke) {
       var loader = new XMLHttpRequest();
 
+      loader.open("GET","carnivores.json");
+      loader.send();
       loader.addEventListener("load", function () {
         // Set the value of the private array
         carnivores = JSON.parse(this.responseText);
@@ -13,8 +15,28 @@ var Predator = (function () {
         // Invoke the callback function so that the caller knows
         // that the process is complete. Make sure to pass the 
         // carnivore array as an argument.
+        callbackToInvoke(carnivores);
 
       });
+    },
+    loadHerbivores: function(callbackToInvoke) {
+      var loader = new XMLHttpRequest();
+
+      loader.open("GET","herbivores.json");
+      loader.send();
+      loader.addEventListener("load", function () {
+        // Set the value of the private array
+        herbivores = JSON.parse(this.responseText);
+
+        // Invoke the callback function so that the caller knows
+        // that the process is complete. Make sure to pass the 
+        // carnivore array as an argument.
+        callbackToInvoke(herbivores);
+
+      });
+
+
+
     }
   }
 })();
