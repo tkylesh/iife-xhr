166196ca9567216b6d960d3ac5e08150a5864d28
Taylor Shuff <tkylesh@gmail.com>
2016-09-29 11:01:56 -0500

    initalizing branchA

---
+++ Predator.js
@@ -0,0 +1,19 @@
+var Predator = (function () {
+  var carnivores = [];
+
+  return {
+    loadCarnivores: function (callbackToInvoke) {
+      var loader = new XMLHttpRequest();
+
+      loader.addEventListener("load", function () {
+        // Set the value of the private array
+        carnivores = JSON.parse(this.responseText);
+
+        // Invoke the callback function so that the caller knows
+        // that the process is complete. Make sure to pass the 
+        // carnivore array as an argument.
+
+      });
+    }
+  }
+})();
